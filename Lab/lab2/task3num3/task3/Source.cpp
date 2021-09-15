246c2467b9eca4cd8c9e9df1577ab55acec1eed6
kr3pt0n <egor20042013@gmail.com>
2021-09-16 01:26:20 +0300

    добавил (cout << "Enter of number's position: ";)

--- Lab/lab2/task3num3/task3/Source.cpp
+++ Lab/lab2/task3num3/task3/Source.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	int k;
-	cout << "Position: ";
+	cout << "Enter of number's position: ";
 	cin >> k;
 	if (k % 2) cout << k / 20 + 1 << endl;
 	else cout << (k / 2 - 1) % 10 << endl;

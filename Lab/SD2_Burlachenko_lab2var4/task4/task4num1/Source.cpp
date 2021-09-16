673443a38427f87c1095b425afc527094bedecda
kr3pt0n <egor20042013@gmail.com>
2021-09-16 23:02:52 +0300

    +task4num1

---
+++ Lab/SD2_Burlachenko_lab2var4/task4/task4num1/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int x, y, z;
+
+int main()
+{
+	setlocale(0, "ukr");
+	cout << "Задача для выяснения существует ли треугольник со сторонами x, y, z." << endl;
+	cout << "x=";
+	cin >> x;
+	cout << "y=";
+	cin >> y;
+	cout << "z=";
+	cin >> z;
+	if ((x + y > z) && (x + y > y) && (y + z > x)) 
+	{
+		cout << "С данными сторонами может существовать треугольник" << endl;
+
+	}
+	else
+	{
+		cout << "С данными сторонами может существовать треугольник" << endl;
+	
+	}
+	
+
+
+	return 0;
+}
+

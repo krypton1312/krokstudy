3c2b87b2cc455e0cfadcab717cc541a946e93b26
kr3pt0n <egor20042013@gmail.com>
2022-10-18 15:35:51 +0200

    c#

---
+++ 3 курс с#/practical/AnimalSolution/Animals/Breed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public enum BREED 
+    { 
+        GOLDEN, 
+        CAIRN, 
+        DANDIE, 
+        SHETLAND, 
+        DOBERMAN, 
+        LAB 
+    }
+}

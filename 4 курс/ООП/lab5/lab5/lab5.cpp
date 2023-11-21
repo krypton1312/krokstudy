250461a7a8ada5658688f313ce3eb6a95f7d7ff6
kr3pt0n <egor20042013@gmail.com>
2023-11-21 13:59:47 +0100

    files

--- 4 курс/ООП/lab5/lab5/lab5.cpp
+++ 4 курс/ООП/lab5/lab5/lab5.cpp
@@ -1,9 +1,13 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <string>;
 using namespace std;
 
 class Currency
 {
 public:
+	fstream fs;
 	virtual double Exchange(double rubl) = 0;
 };
 
@@ -49,19 +53,53 @@ public:
 int main()
 {
 	setlocale(LC_ALL, "ru");
+	fstream fs;
+	string path = "MyFile.txt";
 	try
 	{
 		Dollar dollar;
 		Euro euro;
 		Grivna grivna;
 
+		fs.open(path, fstream::out);
+		fs << "58594";
+		fs.close();
 
-		double rublAmount = 3893;
 
+		fs.open(path, fstream::in | fstream::out | fstream::app);
+		string rublAmountstring = "";
+		
+		fs >> rublAmountstring;
+		fs.seekg(0, ios::beg);
+		int rublAmount = stoi(rublAmountstring);
+		
+		
+		SetConsoleCP(1251);
+		
+		
 		ExchangeService exchangeService;
 		double dollars = exchangeService.ConvertToCurrency(&dollar, rublAmount);
+		fs << "\n" << rublAmount << " рублей в долларах составляет: " << dollars << endl;
+		
 		double euros = exchangeService.ConvertToCurrency(&euro, rublAmount);
+		fs << "\n" << rublAmount << " рублей в евро составляет: " << euros << endl;
+
 		double grivnas = exchangeService.ConvertToCurrency(&grivna, rublAmount);
+		fs << "\n" << rublAmount << " рублей в гривнах составляет: " << grivnas << endl;
+
+
+		SetConsoleCP(866);
+		fs.close();
+
+
+		fs.open(path, fstream::in);
+		cout << endl << endl;
+		string msg = "";
+		while (fs >> msg)
+		{
+			cout << msg << endl;
+		}
+		fs.close();
 	}
 	catch (const std::exception& ex)
 	{
